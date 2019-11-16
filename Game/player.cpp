ef6ab4c334d739ec4955e4aa07208f768affe2dd
Juho <juho.keto-tokoi@tuni.fi>
2019-11-16 13:07:17 +0200

    Removed absolute paths in files

--- Game/player.cpp
+++ Game/player.cpp
@@ -4,7 +4,7 @@ namespace Game{
 Player::Player(const std::string &name, const std::vector<std::shared_ptr<Course::GameObject> > objects)
     : Course::PlayerBase(name,objects)
 {
-    sprite = new QGraphicsPixmapItem(QPixmap("/home/ketotokj/Desktop/Ohjelmointi/Ohjelmointi3/Pirkanmaan_valloitus/juho-ja-leo/Game/Sprites/adamsmith.png"));
+    sprite = new QGraphicsPixmapItem(QPixmap("../../juho-ja-leo/Game/Sprites/adamsmith.png"));
 }
 
 Player::~Player()
